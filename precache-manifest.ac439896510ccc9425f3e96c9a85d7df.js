8403feea77f3737c2576d5cdd72a0d28d75fa2dc
Vincent Sanders <vincesanders@hotmail.com>
2020-07-09 09:42:59 -0500

    Updates

---
+++ precache-manifest.ac439896510ccc9425f3e96c9a85d7df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78455bea131aed9a22bc2fb176222ebb",
+    "url": "/runespeakersedda-under-construction/index.html"
+  },
+  {
+    "revision": "f4163e3a73cdcdd8aca6",
+    "url": "/runespeakersedda-under-construction/static/css/main.b9f306e0.chunk.css"
+  },
+  {
+    "revision": "8955adffc934e91aed27",
+    "url": "/runespeakersedda-under-construction/static/js/2.8a7e2b9f.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/runespeakersedda-under-construction/static/js/2.8a7e2b9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4163e3a73cdcdd8aca6",
+    "url": "/runespeakersedda-under-construction/static/js/main.a70606cf.chunk.js"
+  },
+  {
+    "revision": "7130428ede2631c1eda4",
+    "url": "/runespeakersedda-under-construction/static/js/runtime-main.94151507.js"
+  },
+  {
+    "revision": "1d5f55566c8a79f5977eef209ffd0572",
+    "url": "/runespeakersedda-under-construction/static/media/IMG_2700.1d5f5556.JPG"
+  }
+]);
