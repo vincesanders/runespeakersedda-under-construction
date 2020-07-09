184d2b22a54363a8a02465bcf1eb0fdb56dee025
Vincent Sanders <vincesanders@hotmail.com>
2020-07-08 23:13:01 -0500

    Added image to App with overlay.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
+import treeImage from './assets/IMG_2700.JPG';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Container>
+      <div className='overlay' />
+      <img alt='An image of a large tree.' src={treeImage} />
+    </Container>
   );
 }
 
 const Container = styled.div`
-
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  background-color: #000;
+  overflow: hidden;
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,0.5);
+  }
+  img {
+      /* height: 100%; */
+      width: 80%;
+      margin: 0;
+      padding: 0;
+    }
 `
 
 export default App;
